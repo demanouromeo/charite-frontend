7041677df477f44e58b970b74cc10bc057ca4ec3
Romeo GABIN <gabinprof1@gmail.com>
2026-08-11 20:41:59 -0400

    1.1.1 - preparing for produvtion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:8001/api',
+  apiUrl: 'https://dmsacad.com/charite_backend/api',
   ecole: {
     nom: 'Groupe Scolaire Bilingue La Charité',
     telephone: '+237 45 60 33',
