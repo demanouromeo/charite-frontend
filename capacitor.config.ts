505f9b9c756885251a38f5342357e0edcd33af92
Romeo GABIN <gabinprof1@gmail.com>
2026-08-12 00:52:52 -0400

    adroid added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dmsacad.charite',
+  appName: 'GSB La Charité',
+  webDir: 'dist/frontend/browser'
+};
+
+export default config;
