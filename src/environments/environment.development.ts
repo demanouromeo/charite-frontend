a05abe2032a4e57b6351ae6a15ae49b8d83d96a3
Romeo GABIN <gabinprof1@gmail.com>
2026-08-20 18:26:50 -0400

    minor change

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8010/api',
+  apiUrl: 'http://127.0.0.1:8011/api',
   ecole: {
     nom: 'Groupe Scolaire Bilingue La Charité',
     telephone: '+237 45 60 33',
